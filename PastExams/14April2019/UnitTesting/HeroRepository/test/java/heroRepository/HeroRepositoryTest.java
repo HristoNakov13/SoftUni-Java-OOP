9839355ebd8fb922e8fe6f519e43341afdc5ed8f
HristoNakov13 <admiralbiat@gmail.com>
2019-07-31 16:29:01 +0300

    fixed

--- PastExams/14April2019/UnitTesting/HeroRepository/test/java/heroRepository/HeroRepositoryTest.java
+++ PastExams/14April2019/UnitTesting/HeroRepository/test/java/heroRepository/HeroRepositoryTest.java
@@ -3,31 +3,28 @@ package heroRepository;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class HeroRepositoryTest {
     private static final String PESHO_HERO_NAME = "Pesho";
     private static final int PESHO_HERO_LEVEL_LOWEST = 60;
-    private static final int PESHO_HIGHEST_INT = 10;
-    private static final int PESHO_LOWEST_AGI = 1;
-    private static final int PESHO_HIGHEST_STR = 15;
-    private static final Item PESHO_ITEM = new Item(PESHO_HIGHEST_STR, PESHO_LOWEST_AGI, PESHO_HIGHEST_INT);
+    private static final int PESHO_INT = 10;
+    private static final int PESHO_AGI = 1;
+    private static final int PESHO_STR = 15;
+    private static final Item PESHO_ITEM = new Item(PESHO_STR, PESHO_AGI, PESHO_INT);
     private static final Hero PESHO_HERO = new Hero(PESHO_HERO_NAME, PESHO_HERO_LEVEL_LOWEST, PESHO_ITEM);
     private static final int A_HERO_ADDED_SIZE = 1;
-    private static final int EMPTY_REPOSITORY_SIZE = 0;
     private static final String PESHO_TO_STRING = String.format("Hero: %s – %d%n" +
                     "  *  Strength: %d%n" +
                     "  *  Agility: %d%n" +
                     "  *  Intelligence: %d%n", PESHO_HERO_NAME, PESHO_HERO_LEVEL_LOWEST,
-            PESHO_HIGHEST_STR, PESHO_LOWEST_AGI, PESHO_HIGHEST_INT);
+            PESHO_STR, PESHO_AGI, PESHO_INT);
 
-    private static final String IVAN_HERO_NAME = "Ivan";
-    private static final int IVAN_HERO_LEVEL_LOWEST = 100;
-    private static final int IVAN_LOWEST_INT = 2;
-    private static final int IVAN_HIGHEST_AGI = 20;
-    private static final int IVAN_LOWEST_STR = 3;
-    private static final Item IVAN_ITEM = new Item(IVAN_LOWEST_STR, IVAN_HIGHEST_AGI, IVAN_LOWEST_INT);
-    private static final Hero IVAN_HERO = new Hero(IVAN_HERO_NAME, IVAN_HERO_LEVEL_LOWEST, IVAN_ITEM);
-    private static final Hero DUPLICATE_PESHO = new Hero(PESHO_HERO_NAME, 1,  IVAN_ITEM);
+    private static final Hero NEGATIVE_STR_HERO = new Hero("Hero", 1, new Item(-1, 1, 1));
+    private static final Hero NEGATIVE_AGI_HERO = new Hero("Hero", 1, new Item(1, -1, 1));
+    private static final Hero NEGATIVE_INT_HERO = new Hero("Hero", 1, new Item(1, 1, -1));
+    
 
     private HeroRepository repository;
 
@@ -37,15 +34,8 @@ public class HeroRepositoryTest {
     }
 
     @Test
-    public void shouldReturnCorrectAddedHeroesCount() {
-        Assert.assertEquals(EMPTY_REPOSITORY_SIZE, this.repository.getCount());
-        this.repository.add(PESHO_HERO);
-        Assert.assertEquals(A_HERO_ADDED_SIZE, this.repository.getCount());
-    }
-
-    @Test
     public void shouldAddHeroSuccessfully() {
-        this.repository.add(IVAN_HERO);
+        this.repository.add(PESHO_HERO);
         int expectedSize = A_HERO_ADDED_SIZE;
         Assert.assertEquals(expectedSize, this.repository.getCount());
     }
@@ -53,65 +43,63 @@ public class HeroRepositoryTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowWhenAddingAlreadyExistingHero() {
         this.repository.add(PESHO_HERO);
-        this.repository.add(DUPLICATE_PESHO);
+        this.repository.add(PESHO_HERO);
     }
 
     @Test
     public void shouldRemoveHeroFromRepository() {
-        this.repository.add(IVAN_HERO);
-        this.repository.remove(IVAN_HERO_NAME);
+        this.repository.add(PESHO_HERO);
+        this.repository.remove(PESHO_HERO_NAME);
         int actualSize = this.repository.getCount();
-        Assert.assertEquals(EMPTY_REPOSITORY_SIZE, actualSize);
+        Assert.assertEquals(0, actualSize);
     }
 
     @Test(expected = NullPointerException.class)
     public void shouldThrowWhenRemoveNonExistingHero() {
-        this.repository.add(IVAN_HERO);
         this.repository.remove(PESHO_HERO_NAME);
     }
 
     @Test
     public void shouldReturnCorrectHeroWithHighestStrength() {
         this.repository.add(PESHO_HERO);
-        this.repository.add(IVAN_HERO);
         Hero hero = this.repository.getHeroWithHighestStrength();
-        int actualStrength = hero.getItem().getStrength();
 
-        Assert.assertEquals(PESHO_HIGHEST_STR, actualStrength);
+        Assert.assertEquals(PESHO_HERO, hero);
     }
+
 
     @Test(expected = NullPointerException.class)
     public void shouldThrowWhenNoHigherWithHighestStrengthFound() {
+        this.repository.add(NEGATIVE_STR_HERO);
         this.repository.getHeroWithHighestStrength();
     }
 
     @Test
     public void shouldReturnCorrectHeroWithHighestAgility() {
         this.repository.add(PESHO_HERO);
-        this.repository.add(IVAN_HERO);
         Hero hero = this.repository.getHeroWithHighestAgility();
-        int actualAgility = hero.getItem().getAgility();
 
-        Assert.assertEquals(IVAN_HIGHEST_AGI, actualAgility);
+        Assert.assertEquals(PESHO_HERO, hero);
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldThrowWhenNoHigherWithHighestAgilityFound() {
+    public void shouldThrowWhenNoHeroWithHighestAgilityFound() {
+
+        this.repository.add(NEGATIVE_AGI_HERO);
         this.repository.getHeroWithHighestAgility();
     }
 
     @Test
     public void shouldReturnCorrectHeroWithHighestIntelligence() {
         this.repository.add(PESHO_HERO);
-        this.repository.add(IVAN_HERO);
         Hero hero = this.repository.getHeroWithHighestIntelligence();
-        int actualIntelligence = hero.getItem().getIntelligence();
 
-        Assert.assertEquals(PESHO_HIGHEST_INT, actualIntelligence);
+        Assert.assertEquals(PESHO_HERO, hero);
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldThrowWhenNoHigherWithHighestIntelligenceFound() {
+    public void shouldThrowWhenNoHeroWithHighestIntelligenceFound() {
+        this.repository.add(NEGATIVE_INT_HERO);
         this.repository.getHeroWithHighestIntelligence();
     }
 
